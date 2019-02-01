0bf55213b2d905bc128f7134b6ba20e375a8f122
Alexander Gromov <i2010.grom@yandex.ru>
2019-02-01 17:55:51 +0300

    zap field snake case

--- middleware/zap.go
+++ middleware/zap.go
@@ -4,6 +4,7 @@ package middleware
 import (
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -143,18 +144,16 @@ func ZapWithConfig(cfg ZapConfig) echo.MiddlewareFunc {
 				case "bytes_out":
 					fields = append(fields, zap.Int64(field, res.Size))
 				default:
-					field = strings.ToLower(strings.Replace(field, "-", "_", -1))
-
 					switch {
-					case strings.HasPrefix(field, "header_"):
-						fields = append(fields, zap.String(field, c.Request().Header.Get(field[7:])))
-					case strings.HasPrefix(field, "query_"):
-						fields = append(fields, zap.String(field, c.QueryParam(field[6:])))
-					case strings.HasPrefix(field, "form_"):
-						fields = append(fields, zap.String(field, c.FormValue(field[5:])))
-					case strings.HasPrefix(field, "cookie_"):
+					case strings.HasPrefix(field, "header:"):
+						fields = append(fields, zap.String(snakeCase(field), c.Request().Header.Get(field[7:])))
+					case strings.HasPrefix(field, "query:"):
+						fields = append(fields, zap.String(snakeCase(field), c.QueryParam(field[6:])))
+					case strings.HasPrefix(field, "form:"):
+						fields = append(fields, zap.String(snakeCase(field), c.FormValue(field[5:])))
+					case strings.HasPrefix(field, "cookie:"):
 						if cookie, err := c.Cookie(field[7:]); err == nil {
-							fields = append(fields, zap.String(field, cookie.Value))
+							fields = append(fields, zap.String(snakeCase(field), cookie.Value))
 						}
 					}
 				}
@@ -165,4 +164,30 @@ func ZapWithConfig(cfg ZapConfig) echo.MiddlewareFunc {
 			return err
 		}
 	}
+}
+
+func snakeCase(str string) string {
+	var (
+		runes    = []rune(str)
+		length   = len(runes)
+		alphanum = []*unicode.RangeTable{unicode.Latin, unicode.Digit}
+		out      []rune
+	)
+
+	for i := 0; i < length; i++ {
+		if !unicode.In(runes[i], alphanum...) {
+			if i > 0 && unicode.In(runes[i-1], alphanum...) {
+				out = append(out, '_')
+			}
+			continue
+		}
+		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < length && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
+			if unicode.In(runes[i-1], alphanum...) {
+				out = append(out, '_')
+			}
+		}
+		out = append(out, unicode.ToLower(runes[i]))
+	}
+
+	return string(out)
 }
